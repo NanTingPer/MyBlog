97520ba273aaa4eebdbc7278083072d41f630ac2
Nan_Ting <a3380991070@163.com>
2026-04-17 17:18:28 +0800

    frontend: 主页列表渲染

--- src/frontend/src/index.ts
+++ src/frontend/src/index.ts
@@ -4,18 +4,47 @@
 import { BaseRequestModel } from "./types/BaseRquestModel.js";
 import { BlogInfo } from "./types/blogs/BlogInfo.js";
 import { BlogAPI } from "./utils/BlogAPI.js";
+import { DateTimeFormat } from "./utils/DateTimeFormat.js";
 
 // 运行项目: npm install -g http-server
 //          http-server -p 80
-export async function sayHelloWorld(world: string) {
-    let _docuemnt = document.createElement('a');
-    _docuemnt.innerHTML = "恭喜加载了";
+export async function reanderBlogList(mountId: string) {
     var blogAPI = new BlogAPI("http://localhost:5162");
     let searchResponse = await blogAPI.Search();
-    if(searchResponse.status == 200) {
-        let json = await searchResponse.json();
-        let blogs = json as BaseRequestModel<BlogInfo[]>
-        console.log(blogs);
+    if(searchResponse.status != 200) {
+        return;
     }
-    return `Hello ${world}`;
+
+    let json = await searchResponse.json();
+    let blogs = json as BaseRequestModel<BlogInfo[]>
+    if(blogs.code != 200) return;
+    let element = document.getElementById(mountId);
+    if (element == undefined) {
+        console.error(`未找到${mountId}, 无法挂载博文`);
+        return;
+    }
+    let blogHtml = "";
+    const titleClassName = "post-title";
+    blogs.data?.forEach(blog => {
+        let createDate = new DateTimeFormat(blog.createTime);
+        // <span class="read-time">阅读时长: 2分钟</span>
+        blogHtml +=  
+                `<article class="blog-post">
+                    <h2 class="${titleClassName}" onclick="reanderBlog('${mountId}', '${blog.id}')">${blog.name}</h2>
+                    <div class="post-meta">
+                        <span class="post-date">${createDate.Year()}/${createDate.Month()}/${createDate.Date()} ovo ${createDate.Hours()}:${createDate.Min()}</span>
+                    </div>
+                </article>`
+    });
+    element.innerHTML = blogHtml;
+}
+
+export async function reanderBlog(mountId: string, blogId: string) {
+    let blogAPI = new BlogAPI("http://localhost:5162");
+    let response = await blogAPI.SearchOnId({ keyWord: blogId });
+    if (response.status != 200) return;
+    let requestValue = (await response.json()) as BaseRequestModel<BlogInfo>;
+    if (requestValue.code != 200 || requestValue.data == undefined) return;
+    let element = document.getElementById(mountId);
+    element!.innerHTML = requestValue.data.content;
 }
---
+++ src/frontend/src/utils/DateTimeFormat.ts
@@ -0,0 +1,36 @@
+﻿export class DateTimeFormat{
+    public readonly utcTick: number; 
+    private readonly dateTime: Date;
+    
+    constructor(utcTick: number) {
+        const dotNetTicks = 6391194406543387;
+        const dotNetEpochTicks = 6213559680000000;
+        const millisecondsSinceEpoch = (dotNetTicks - dotNetEpochTicks) / 100;
+        this.utcTick = utcTick;
+        this.dateTime = new Date(millisecondsSinceEpoch);
+    }
+
+    public Year() {
+        return this.dateTime.getFullYear();
+    }
+
+    public Month() {
+        return this.To2(String(this.dateTime.getMonth()));
+    }
+
+    public Date() {
+        return this.To2(String(this.dateTime.getDate()));
+    }
+
+    public Hours() {
+        return this.To2(String(this.dateTime.getHours()));
+    }
+
+    public Min() {
+        return this.To2(String(this.dateTime.getMinutes()));
+    }
+    private To2(value: string) {
+        if(value.length < 2) return '0' + value
+        else return value;
+    }
+}
