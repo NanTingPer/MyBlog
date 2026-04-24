3d3f3df09ca1ed6b6ed2b00cced43a42fbe9d6f3
Nan_Ting <a3380991070@163.com>
2026-04-24 21:37:36 +0800

    后端: efcore的迁移内容

---
+++ src/backend/NanTingBlog.API/Migrations/20260424123956_友链表.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NanTingBlog.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class 友链表 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "friendslink",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "text", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    url = table.Column<string>(type: "text", nullable: false),
+                    dictum = table.Column<string>(type: "text", nullable: false),
+                    avatar = table.Column<string>(type: "text", nullable: false),
+                    createUnixEpochTick = table.Column<long>(type: "bigint", nullable: false),
+                    createTime = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_friendslink", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "friendslink");
+        }
+    }
+}
