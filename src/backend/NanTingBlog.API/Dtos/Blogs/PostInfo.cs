0af9b8cc59df198e13b7ae01515ee8df6ac7b967
Nan_Ting <a3380991070@163.com>
2026-05-14 18:54:19 +0800

    后端: 博文实体新增title和description字段 并进行数据库迁移

--- src/backend/NanTingBlog.API/Dtos/Blogs/PostInfo.cs
+++ src/backend/NanTingBlog.API/Dtos/Blogs/PostInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
@@ -14,12 +15,36 @@ public class PostInfo
 #pragma warning disable CS1591 // 缺少对公共可见类型或成员的 XML 注释
     [JsonPropertyName("id"), Column("id"), Key]
     public string Id { get; set; } = Guid.NewGuid().ToString();
+    
+    /// <summary>
+    /// 文章的文件名
+    /// </summary>
     [JsonPropertyName("name"), Column("name")]
     public string Name { get; set; } = string.Empty;
+    
+    /// <summary>
+    /// 文章显示的文章名称
+    /// </summary>
+    [JsonPropertyName("title"), Column("title")]
+    public string Title { get; set; } = string.Empty;
+
+    /// <summary>
+    /// 文章显示的描述信息
+    /// </summary>
+    [JsonPropertyName("description"), Column("description"), Description("文章显示的描述信息")]
+    public string Description { get; set; } = string.Empty;
+    
+    /// <summary>
+    /// 文章的创建日期，以1970年1月1日开始
+    /// </summary>
     [JsonPropertyName("createTime"), Column("createTime")]
     public long CreateTime { get; set; } = DateTime.UtcNow.Ticks - DateTimeOffset.UnixEpoch.Ticks;
     [JsonPropertyName("editTime"), Column("editTime")]
     public long EditTime { get; set; } = DateTime.UtcNow.Ticks - DateTimeOffset.UnixEpoch.Ticks;
+
+    /// <summary>
+    /// 文章作者
+    /// </summary>
     [JsonPropertyName("author"), Column("author")]
     public List<string> Author { get; set; } = [];
     [JsonPropertyName("content"), Column("content")]
---
+++ src/backend/NanTingBlog.API/Migrations/20260514060620_Post table add title and description field.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NanTingBlog.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Posttableaddtitleanddescriptionfield : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "description",
+                table: "posts",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "title",
+                table: "posts",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "description",
+                table: "posts");
+
+            migrationBuilder.DropColumn(
+                name: "title",
+                table: "posts");
+        }
+    }
+}
