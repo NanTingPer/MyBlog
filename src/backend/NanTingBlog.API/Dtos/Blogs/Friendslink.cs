228cb2523dfa9c8e078752e7b32c144af0837fd1
Nan_Ting <a3380991070@163.com>
2026-06-20 19:47:14 +0800

    fix: [后端] 友链user导航属性导致的前端数据无法验证

--- src/backend/NanTingBlog.API/Dtos/Blogs/Friendslink.cs
+++ src/backend/NanTingBlog.API/Dtos/Blogs/Friendslink.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -84,6 +85,7 @@ public class Friendslink
     /// 用户查询结果
     /// </summary>
     [JsonIgnore]
+    [ValidateNever]
     public User User { get; set; }
 #pragma warning restore CS8618 // 在退出构造函数时，不可为 null 的字段必须包含非 null 值。请考虑添加 "required" 修饰符或声明为可为 null。
 }
